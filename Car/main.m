d8a7920a3c83fce86a547bf2cde6cc3067c50af9
thodep <thodep@yahoo.com>
2015-04-20 14:10:29 -0400

    Initial Commit

---
+++ Car/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Car
+//
+//  Created by tho dang on 2015-04-20.
+//  Copyright (c) 2015 TD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
